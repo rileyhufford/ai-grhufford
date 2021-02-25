d7c32c82a52dbc3c61531f18e2760eb3847d0065
Riley Hufford <rileyhufford@outlook.com>
2021-02-25 15:46:32 -0800

    corner goat agent started

---
+++ baghchal/cornergoatagent.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple, Optional
+from agent import Agent
+from game import Game
+from const import Const
+from move import Move
+import random
+
+class cornergotagent(Agent)
+{
+
+
+}
